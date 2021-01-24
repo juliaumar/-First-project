88f53f356fa86a50bd26948a7da620878831a320
juliaumar <juliaumar71@gmail.com juliaumar71@gmail.com git config --global user.email juliaumar71@gmail.comgit config --global user.email juliaumar71@gmail.com git config --global user.name Julia>
2021-01-24 14:14:22 -0500

    Done

--- src/MyFirstProject.java
+++ src/MyFirstProject.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class MyFirstProject {
+public class MyFirstProject {
+    public static void main(String[] args) {
+        System.out.println("My first project will be successfulgit");
+
+    }
 }
