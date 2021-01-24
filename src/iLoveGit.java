b52693b03c8d08ce58f2253a9456477010bb0837
juliaumar <juliaumar71@gmail.com juliaumar71@gmail.com git config --global user.email juliaumar71@gmail.comgit config --global user.email juliaumar71@gmail.com git config --global user.name Julia>
2021-01-24 14:21:06 -0500

    Yes

---
+++ src/iLoveGit.java
@@ -0,0 +1,20 @@
+public class iLoveGit {
+    public static void main(String[] args) {
+        System.out.println("\t1. Create project with IntelliJ\n" +
+                "\t2. Initilize as git project: \n" +
+                "\t   - navigate to root level of project(or do from IntelliJ terminal)\n" +
+                "\t   - git init\n" +
+                "\t3. Start working on code and do commit\n" +
+                "\t   - git add <file_path> or git add .\n" +
+                "\t   - git commit -m \"your message\"\n" +
+                "\t4. Create repository in GitHub\n" +
+                "\t   - name should be same\n" +
+                "\t5. git remote add origin <repository_url> \n" +
+                "\t6. git push -u origin master\n" +
+                "\t7. If you changed the code:\n" +
+                "\t   git add <fiel_path> or git add . \n" +
+                "\t   git commit -m \"message\"\n" +
+                "\t   git push");
+
+    }
+}
